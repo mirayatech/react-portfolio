c0dcc66b624b9aa5dc35b9c1f83c0f75f5201ddf
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-24 21:21:25 +0100

    ✨ initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function App() {
+  return <div>App</div>
+}
---
+++ src/library/colors.ts
@@ -0,0 +1,32 @@
+export const lightModeColors = {
+  white: '#fff',
+  body: '#F9FAFB',
+  black: '#1a202c',
+  badge: '#EAEAEA',
+  darkGrey: '#4444',
+  lightGrey: '#6666',
+  toggleBorder: '#999999',
+}
+
+export const darkModeColors = {
+  white: '#fff',
+  body: '#202023',
+  badge: '#444444',
+  darkGrey: '#3333',
+  lightGrey: '#888888',
+  toggleBorder: '#999999',
+}
+
+export const linearGardient = {
+  1: `linear-gradient(180deg,#f3cbab,#feedca)`,
+  2: `linear-gradient(180deg,#b1e5f9,#f4d2fe)`,
+  3: `linear-gradient(180deg,#dbb4f3,#efb7d7)`,
+  4: `linear-gradient(180deg,#efa971,#e4cafe)`,
+  5: `linear-gradient(180deg,#A88BEB,#F8CEEC)`,
+  6: `linear-gradient(180deg,#FFD8CB,#F9D29D)`,
+  7: `linear-gradient(180deg,#F9D976,#F39F86)`,
+  9: `linear-gradient(180deg,#B0F3F1,#FFCFDF)`,
+  10: `linear-gradient(180deg,#9795EF,#F9C5D1)`,
+  11: `linear-gradient(180deg,#AFF1DA,#F9EA8F)`,
+  12: `linear-gradient(180deg,#FE5F75,#FC9842)`,
+}
