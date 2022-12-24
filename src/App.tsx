30cac971a64828c2f568edef8fd99d67410cdb36
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-24 21:35:30 +0100

    ℹ️ coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: '#F6F2EF',
+      }}
+    >
+      <img
+        src="/comingsoon.png"
+        alt=""
+        style={{
+          width: '310px',
+        }}
+      />
+    </div>
+  )
 }
