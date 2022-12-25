7f6b503162f7fa9e78d570629bc741f25da4fb56
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 09:06:19 +0100

    style: add styling to toggle button

---
+++ src/feautures/toggle-theme/toggle-theme-style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { flexCenter } from '../../library'
+
+export const Button = styled.button`
+  ${flexCenter}
+  padding: 0.3rem;
+  font-size: 1.25rem;
+  border-radius: 0.3rem;
+`
