3bc58d800fe650fb5cd048e0f4f8dd54af3eae3d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 17:54:59 +0100

    feat: more icons

--- src/library/icons.ts
+++ src/library/icons.ts
@@ -1,3 +1,6 @@
 import { BiSun as Sun, BiMoon as Moon } from 'react-icons/bi'
+import { VscGithub as Github } from 'react-icons/vsc'
+import { RxExternalLink as Link } from 'react-icons/rx'
+import { MdOutlineHorizontalRule as Line } from 'react-icons/md'
 
-export { Sun, Moon }
+export { Sun, Moon, Github, Link, Line }
