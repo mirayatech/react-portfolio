5a38bb4cc94f213e6e5f46dc5531d332e8baedfa
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 19:01:38 +0100

    remove: toggle theme button

--- src/feautures/toggle-theme/toggle-theme-style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { flexCenter } from '../../library'
-
-export const Button = styled.button`
-  ${flexCenter}
-  padding: 0.3rem;
-  font-size: 1.25rem;
-  border-radius: 0.3rem;
-`
