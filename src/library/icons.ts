3da85c14793c799bc1dec032223e0dbdf4cd796a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 18:47:05 +0100

    feat: add more icons

--- src/library/icons.ts
+++ src/library/icons.ts
@@ -2,5 +2,9 @@ import { BiSun as Sun, BiMoon as Moon } from 'react-icons/bi'
 import { VscGithub as Github } from 'react-icons/vsc'
 import { RxExternalLink as Link } from 'react-icons/rx'
 import { MdOutlineHorizontalRule as Line } from 'react-icons/md'
+import { BsInstagram as Instagram } from 'react-icons/bs'
+import { RiTwitterLine as Twitter } from 'react-icons/ri'
+import { IoLogoYoutube as YouTube } from 'react-icons/io'
+import { FaTiktok as TikTok } from 'react-icons/fa'
 
-export { Sun, Moon, Github, Link, Line }
+export { Sun, Moon, Github, Link, Line, Instagram, YouTube, TikTok, Twitter }
