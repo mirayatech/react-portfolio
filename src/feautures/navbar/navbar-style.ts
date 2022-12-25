b3f6a09c0f44dd091ba5aec244faafb11c85bed0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 13:17:53 +0100

    style: small style changes on navbar

--- src/feautures/navbar/navbar-style.ts
+++ src/feautures/navbar/navbar-style.ts
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
 
 export const Nav = styled.nav`
+  width: 100%;
+  position: fixed;
+  padding: 1rem 1.4em;
+
+  backdrop-filter: blur(0.8rem);
+  -webkit-backdrop-filter: blur(0.8rem);
+`
+
+export const Wrapper = styled.div`
+  margin: auto;
   display: flex;
+  max-width: 75rem; //1200px
   align-items: center;
   justify-content: space-between;
 `
 
 export const Links = styled.div``
 
-export const Link = styled.a`
-  margin-right: 1rem;
+export const Link = styled.span`
+  cursor: pointer;
+  margin-right: 1.5rem;
 `
