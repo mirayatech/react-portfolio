4a1425b162210dba6e25274c0cdc7b0cd98882ab
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 11:13:29 +0100

    style: add styling to navbar

---
+++ src/feautures/navbar/navbar-style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Links = styled.div``
+
+export const Link = styled.a`
+  margin-right: 1rem;
+`
