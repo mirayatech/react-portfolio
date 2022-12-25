b409aff84aaa3f6763286b0b11f4ceccf4fce465
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-25 17:56:07 +0100

    feat: another layout property

---
+++ src/page/app-style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Primary = styled.div`
+  margin: auto;
+  height: 100vh;
+  width: 75rem;
+  display: flex;
+  align-items: center;
+`
+
+export const Secondary = styled.div`
+  margin: auto;
+  width: 75rem;
+  display: flex;
+  align-items: center;
+`
