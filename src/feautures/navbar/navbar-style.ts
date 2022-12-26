9d4bff2bf9b6bf26ab7cd8ee99bf3932682b8ce3
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-26 17:42:03 +0100

    style: add styling to navbar

--- src/feautures/navbar/navbar-style.ts
+++ src/feautures/navbar/navbar-style.ts
@@ -1,30 +1,180 @@
 import styled from 'styled-components'
-import { colors } from '../../library'
+import { colors, flexCenter } from '../../library'
+const H1 = `
+font-weight: 900;
+    letter-spacing: 2px;
+    font-size: 0.95rem;
+    color: ${colors.white};
+    text-transform: uppercase;
+    margin-bottom: 1rem;
+    background: linear-gradient(
+      -45deg,
+      ${colors.red},
+      ${colors.blue},
+      ${colors.red},
+      ${colors.blue}
+    );
+    background-clip: text;
+    background-size: 400% 400%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: gradient 5s ease infinite;
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+    @media screen and (max-width: 745px) {
+      font-size: .8rem;
+    }
+    `
+
+const H2 = `
+font-weight: 900;
+    letter-spacing: 2px;
+    font-size: 0.95rem;
+    color: ${colors.white};
+    text-transform: uppercase;
+    margin-bottom: 1rem;
+    background: linear-gradient(
+      -45deg,
+      ${colors.red},
+      ${colors.blue},
+      ${colors.green},
+      ${colors.blue}
+    );
+    background-clip: text;
+    background-size: 400% 400%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: gradient 5s ease infinite;
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+      @media screen and (max-width: 745px) {
+      font-size: .8rem;
+    }
+    `
 
 export const Nav = styled.nav`
-  width: 100%;
+  display: flex;
+  padding: 2rem;
+  position: relative;
+  justify-content: flex-end;
+
+  button {
+    position: fixed;
+    z-index: 20;
+    ${flexCenter}
+    width: 3rem;
+    height: 3rem;
+    border: none;
+    font-size: 2.5rem;
+    border-radius: 50%;
+    color: ${colors.white};
+    background: transparent;
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+
+    transition: all 0.2s ease;
+
+    :hover {
+      transform: scale(1.1);
+    }
+
+    svg {
+      height: 2.8rem;
+    }
+  }
+`
+
+export const Menu = styled.nav`
+  top: 0;
+  left: 0;
   z-index: 10;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  overflow: auto;
   position: fixed;
-  padding: 1rem 1.4em;
-  backdrop-filter: blur(0.8rem);
-  -webkit-backdrop-filter: blur(0.8rem);
+  align-items: center;
+  justify-content: space-evenly;
+  flex-direction: row-reverse;
+  background-color: #00000001;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+
+  @media screen {
+    flex-direction: column;
+  }
+
+  * {
+    z-index: 20;
+  }
+`
+
+export const Navigation = styled.div`
+  h1 {
+    ${H1}
+  }
 `
 
 export const Wrapper = styled.div`
-  margin: auto;
   display: flex;
-  max-width: 75rem; //1200px
-  align-items: center;
-  justify-content: space-between;
+  line-height: 2.5;
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: ${colors.white};
+  flex-direction: column;
+
+  @media screen and (max-width: 745px) {
+    font-size: 1rem;
+  }
+  span {
+    cursor: pointer;
+    transition: all 0.2s ease;
+  }
+  span:hover {
+    transform: scale(1.1);
+  }
 `
 
-export const Links = styled.div``
+export const Links = styled.div`
+  h1 {
+    ${H2}
+  }
+`
 
-export const Link = styled.span`
-  cursor: pointer;
-  font-weight: 500;
-  font-size: 0.9rem;
-  margin-right: 1.5rem;
-  text-transform: uppercase;
-  color: ${colors.offWhite};
+export const Grid = styled.div`
+  display: grid;
+  line-height: 2.5;
+  grid-template-columns: auto auto;
+
+  a {
+    color: ${colors.white};
+    font-size: 1.6rem;
+    cursor: pointer;
+    transition: all 0.2s ease;
+
+    @media screen and (max-width: 745px) {
+      font-size: 1.4rem;
+    }
+  }
+  a:hover {
+    transform: scale(1.1);
+  }
 `
