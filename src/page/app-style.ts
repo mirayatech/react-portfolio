80677806c973b253035fe4a054277f771da81f0a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-26 09:29:22 +0100

    style: small styling changes

--- src/page/app-style.ts
+++ src/page/app-style.ts
@@ -2,26 +2,23 @@ import styled from 'styled-components'
 
 export const Primary = styled.div`
   margin: auto;
-  height: 100vh;
   width: 75rem;
+  height: 100vh;
   display: flex;
   align-items: center;
 `
 
 export const Secondary = styled.div`
-  margin: auto;
-  width: 75rem;
   display: flex;
   align-items: center;
+  padding: 15px 15px 100px 15px;
 `
 
 export const Tertiary = styled.div`
-  width: 75rem;
-  margin: auto;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 15px 15px 100px 15px;
 `
 
 export const ContactWrapper = styled.div`
