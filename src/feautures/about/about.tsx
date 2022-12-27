59f2cd504e41d46486cc9a2e0539c9d27e5c383f
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-27 18:28:51 +0100

    feat: create about component

---
+++ src/feautures/about/about.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Headings } from '../headings/headings'
+import { Link, Text } from './about-style'
+
+export default function About() {
+  const title = 'About'
+  const subtitle = "Hi, I'm Miraya."
+
+  return (
+    <div>
+      <Headings title={title} subtitle={subtitle} />
+
+      <Text>
+        I&#39;m a 20 year old self taught frontend developer. I discovered my
+        passion for programming after discovering my brother was doing it. What
+        kept me on this journey is that I always find it exciting to learn new
+        things and challenges I&#39;m unfamiliar with. I currently work as a
+        front end developer at{' '}
+        <Link href="https://www.check24.de/">CHECK24</Link>. When I&#39;m not
+        doing anything coding-related, I enjoy spending time with my family and
+        working out.
+      </Text>
+    </div>
+  )
+}
