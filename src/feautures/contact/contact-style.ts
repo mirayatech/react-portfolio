103ad9e8790fa7dd84a84e9f0b5f4ddae6f2e41a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-27 18:30:25 +0100

    small fixes

--- src/feautures/contact/contact-style.ts
+++ src/feautures/contact/contact-style.ts
@@ -4,6 +4,7 @@ import { colors } from '../../library'
 export const Text = styled.p`
   max-width: 800px;
   font-size: 1.1rem;
+  line-height: 2;
   color: ${colors.lightGrey};
 
   @media screen and (max-width: 745px) {
