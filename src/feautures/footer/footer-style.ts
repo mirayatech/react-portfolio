d2d544be2866a99dbf01653804ee84669d49319c
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-27 18:29:54 +0100

    style: more padding bottom

--- src/feautures/footer/footer-style.ts
+++ src/feautures/footer/footer-style.ts
@@ -3,7 +3,7 @@ import { colors } from '../../library'
 
 export const Wrapper = styled.div`
   display: flex;
-  padding: 1rem;
+  padding: 1rem 1rem 2rem;
   font-weight: 600;
   font-size: 0.7rem;
   letter-spacing: 1px;
